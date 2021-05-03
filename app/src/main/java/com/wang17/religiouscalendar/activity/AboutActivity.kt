b84dfeef90915289d02201ef7462d79014cba42e
wangsc <645708679@qq.com>
2021-05-03 10:07:53 +0800

    1

--- app/src/main/java/com/wang17/religiouscalendar/activity/AboutActivity.kt
+++ app/src/main/java/com/wang17/religiouscalendar/activity/AboutActivity.kt
@@ -27,15 +27,9 @@ class AboutActivity : AppCompatActivity() {
         tv_about.text="Copyright @ 2012-${DateTime.getToday().getYear()} 王世超. "
         // 下载
         tv_loadUrl.setOnClickListener {
-            AlertDialog.Builder(this).setCancelable(false).setMessage(resources.getString(R.string.load_url_introduce)).setPositiveButton("前往", DialogInterface.OnClickListener { dialog, which ->
-            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(resources.getString(R.string.load_url))))
-            }).show()
-//            AlertDialog.Builder(this).setCancelable(false).setMessage("点击下载rc.apk文件，并安装。").setPositiveButton("下载", DialogInterface.OnClickListener { dialog, which ->
-//                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://7969-yipinshangdu-4wk7z-1301432092.tcb.qcloud.la/rc.apk?sign=918e5ea1851eebfd545295ae4762ddc1&t=1618279177")))
-//            }).show()
-//            val intent = Intent(this, WebActivity::class.java)
-//            intent.putExtra("url", resources.getString(R.string.load_url))
-//            startActivity(intent)
+            AlertDialog.Builder(this).setCancelable(false).setMessage(resources.getString(R.string.load_url_introduce)).setPositiveButton("前往") { dialog, which ->
+                startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(resources.getString(R.string.load_url))))
+            }.show()
         }
         // 联系我们
         tv_contact.setOnClickListener {
@@ -44,7 +38,6 @@ class AboutActivity : AppCompatActivity() {
         // 隐私保护
         tv_info.setOnClickListener {
             startActivity(Intent(Intent.ACTION_VIEW, Uri.parse(resources.getString(R.string.home_url))))
-//            startActivity(Intent(this, WebActivity::class.java))
         }
     }
 }
